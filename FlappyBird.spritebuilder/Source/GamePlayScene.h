2c68459ae794acca7de4a3c85f9e833b717eae4b
Mark Kelly <marcus@Marks-MacBook.local>
2015-05-23 17:30:59 +0100

    Automatically Committed for MakeGamesWithUs

--- FlappyBird.spritebuilder/Source/GamePlayScene.h
+++ FlappyBird.spritebuilder/Source/GamePlayScene.h
@@ -18,9 +18,6 @@ typedef NS_ENUM(NSInteger, DrawingOrder) {
 @interface GamePlayScene : CCNode <CCPhysicsCollisionDelegate>
 {
     // define variables here;
-    Character*     character;
-    CCPhysicsNode* physicsNode;
-    float timeSinceObstacle;
 }
 
 -(void) initialize;
--- FlappyBird.spritebuilder/Source/GamePlayScene.m
+++ FlappyBird.spritebuilder/Source/GamePlayScene.m
@@ -7,32 +7,13 @@
 - (void)initialize
 {
     // your code here
-    character = (Character*)[CCBReader load:@"Character"];
-    [self addObstacle];
-    timeSinceObstacle = 0.0f;
 }
 
-- (void)update:(CCTime)delta
-{// Increment the time since the last obstacle was added
-    timeSinceObstacle += delta; // delta is approximately 1/60th of a second
-    
-    // Check to see if two seconds have passed
-    if (timeSinceObstacle > 2.0f)
-    {
-        // Add a new obstacle
-        [self addObstacle];
-        
-        // Then reset the timer.
-        timeSinceObstacle = 0.0f;
-    }
-    // this will be run every frame.
-    // delta is the time that has elapsed since the last time it was run. This is usually 1/60, but can be bigger if the game slows down
+-(void)update:(CCTime)delta
+{
+    // put update code here
 }
 
 // put new methods here
-- (void)touchBegan:(CCTouch *)touch withEvent:(CCTouchEvent *)event{
-    // this will get called every time the screen is touched
-    [character flap];
-}
 
 @end
